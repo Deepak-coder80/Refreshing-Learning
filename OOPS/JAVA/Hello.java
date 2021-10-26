999faad8d30380abb56b3ab82c768afb5d0658f1
Deepak M S <souparnikad40@gmail.com>
2021-10-26 19:58:24 +0530

    oops started

---
+++ OOPS/JAVA/Hello.java
@@ -0,0 +1,22 @@
+package OOPS.JAVA;
+
+public class Hello {
+    public static void main(String[] args) {
+        // create an object for sample class
+
+    }
+}
+
+class Sample {
+    int a;
+    int b;
+    int c;
+
+    void calculateSum() {
+        c = a + b;
+    }
+
+    void dispaly() {
+        System.out.println("a=" + a + "\nb=" + b + "\nsum =" + c);
+    }
+}
