b1253cb745c61f4b305f7212987caf304d8eecf8
Deepak M S <souparnikad40@gmail.com>
2021-10-26 21:17:30 +0530

    static methode 1 finished

---
+++ OOPS/JAVA/Static.java
@@ -0,0 +1,18 @@
+package OOPS.JAVA;
+
+public class Static {
+    int a = 10;
+    //static int a=10;//-->METHOD 1
+    public static void main(String[] args) {
+        // System.out.println(a);-->this will produce an error because
+        // variable a is not a static variable
+
+        /*
+        This can be fixed in two ways
+        1. make the variable as static one
+        2. create an object for this class
+        */
+        // System.out.println(a); --> Method1
+
+    }
+}
