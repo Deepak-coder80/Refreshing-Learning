dba9dbff6e1a5d7175a67cf67217025cbf19bfa2
Deepak M S <souparnikad40@gmail.com>
2021-10-30 21:16:27 +0530

    member 1 first method added

--- OOPS/CPP/class1.cpp
+++ OOPS/CPP/class1.cpp
@@ -5,20 +5,36 @@ using namespace std;
 class Box{
     //public data members
     public:
-        int height;
-        int length;
-        int width;
+        double height;
+        double length;
+        double width;
+        /*
+        Member function: function inside a class
+        delcaration : inside function
+        definition: 2 methodes
+            1.inside the class 
+            2. using scope resolution operator(::)
+        */
+       //METHOD 1
+       double getVolume(){
+           return length*height*width;
+       }
 };
 
 int main(){
     //object for Box class
     Box box1;
 
-    box1.height=33;
-    box1.length=33;
-    box1.width=33;
+    box1.height=3;
+    box1.length=3;
+    box1.width=3;
 
     cout<<"Box Height : "<<box1.height<<endl;
     cout<<"Box Length : "<<box1.length<<endl;
     cout<<"Box Width : "<<box1.width<<endl;
+
+    double volume = box1.getVolume();
+
+    cout<<"Volume of the box : "<<volume<<endl;
+
 }
